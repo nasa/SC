6cf2283ceb0118daa0c27aaa287766bc01366fbf
Dylan Baker <dylan.z.baker@nasa.gov>
2026-05-12 15:02:55 -0400

    Part #686, Update version numbers for v7.0.1

--- fsw/src/sc_version.h
+++ fsw/src/sc_version.h
@@ -32,7 +32,7 @@
 
 #define SC_MAJOR_VERSION 7 /**< \brief Major version number */
 #define SC_MINOR_VERSION 0 /**< \brief Minor version number */
-#define SC_REVISION      0 /**< \brief Revision number */
+#define SC_REVISION      1 /**< \brief Revision number */
 
 /**\}*/
 
